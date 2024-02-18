0bd4a13a70c3ad044c28c2c7fabaa5ec1d50f60d
Akemi <eka.budhiprasetya@gmail.com>
2024-02-18 19:52:31 +0800

    feat: finish welcome screen

---
+++ NewAppTpp/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using HandyControl.Tools;
+using NewAppTpp.MVVM.Model;
+using NewAppTpp.Services;
+
+namespace NewAppTpp.MVVM.ViewModel
+{
+    internal class HomeViewModel : BindablePropertyBase
+    {
+        private readonly UserAccessModel _userAccessModel = new();
+
+        public string Nama
+        {
+            get { return _userAccessModel.Nama; }
+            set { _userAccessModel.Nama = value; RaisePropertyChanged(nameof(Nama)); }
+        }
+
+        public HomeViewModel() 
+        {
+            Nama = UserAccessService.Instance.CurrentNama;
+        }
+    }
+}
